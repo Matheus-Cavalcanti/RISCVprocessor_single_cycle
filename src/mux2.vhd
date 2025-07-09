7c5f86964e61ba44bec6f84d0dc57a1d510f56ae
Matheus Cavalcanti de Santana <132709977+Matheus-Cavalcanti@users.noreply.github.com>
2025-07-09 16:01:38 -0300

    Update mux2.vhd

--- src/mux2.vhd
+++ src/mux2.vhd
@@ -1,7 +1,8 @@
 ENTITY mux2 IS
-	PORT (d0, d1 : IN BIT_VECTOR(31 DOWNTO 0); --entradas (32 bits)
-			s		 : IN BIT;  							         --seletor
-			y		 : OUT BIT_VECTOR(31 DOWNTO 0));   --saida (32 bits)
+	GENERIC (width	 : INTEGER := 32);
+	PORT	  (d0, d1 : IN BIT_VECTOR(width-1 DOWNTO 0);	  --entradas (32 bits)
+				s		 : IN BIT;									  --seletor
+				y		 : OUT BIT_VECTOR(width-1 DOWNTO 0)); --saida (32 bits)
 END mux2;
 
 ARCHITECTURE logic OF mux2 IS
