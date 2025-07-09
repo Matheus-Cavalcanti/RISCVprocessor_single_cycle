ddeace44ad134d17efc1ba345e29d56dccf48665
Matheus Cavalcanti de Santana <132709977+Matheus-Cavalcanti@users.noreply.github.com>
2025-07-09 16:12:05 -0300

    Update imem.vhd

--- src/imem.vhd
+++ src/imem.vhd
@@ -1,8 +1,9 @@
-LIBRARY bibli_1;
-USE bibli_1.riscv_pkg.ALL;
+library bibli_1;
+use bibli_1.RISCV_PKG.all;
 
 library IEEE;
 use ieee.std_logic_textio.all;
+use IEEE.NUMERIC_BIT.all;		-- Used for rising_edge function
 
 use STD.TEXTIO.all;
 
@@ -16,33 +17,27 @@ architecture behave of imem is
 				BIT_VECTOR(31 downto 0);
 	-- initialize memory from file
 	impure function init_ram_hex return ramtype is
-		 -- Ajuste esse caminho se necessário de acordo com sua pasta de simulação:
-		 file text_file : text open read_mode is "C:\Users\Mathe\OneDrive\Documentos\SEL0632\riscvsingle\src\riscvtest.txt";
-		 variable text_line  : line;
-		 variable ram_content: ramtype;
-	begin
-    -- 1) Zera toda a memória (0..63)
-    for i in ram_content'range loop
-        ram_content(i) := (others => '0');
-    end loop;
-
-    -- 2) Lê NO MÁXIMO 64 linhas, colocando cada uma em ram_content(0), ram_content(1), …
-    for i in ram_content'range loop
-        if not endfile(text_file) then
-            readline(text_file, text_line);
-            hread(text_line, ram_content(i));
-        else
-            exit;  -- acabou o arquivo, sai do loop
-        end if;
-    end loop;
-
-    return ram_content;
+		file text_file : text open read_mode is "../src/riscvtest.txt";
+		variable text_line : line;
+		variable ram_content : ramtype;
+		variable i : integer := 0;
+		begin
+		for i in 0 to 63 loop -- set all contents low
+			ram_content(i) := (others => '0');
+		end loop;
+		while not endfile(text_file) loop -- set contents from file
+			readline(text_file, text_line);
+			hread(text_line, ram_content(i));		-- Function do pacote TEXTIO da versao 2008 do VHDL -- Leitura de valores em hexadecimal
+			i := i + 1;
+		end loop;
+		
+		return ram_content;
 	end function;
 	
 	signal mem : ramtype := init_ram_hex;
 	begin
 	-- read memory
 	process(a) begin
-		rd <= mem(BTOI(a(31 downto 2)));
+		rd <= mem(to_integer(unsigned(a(31 downto 2))));
 	end process;
 end;
