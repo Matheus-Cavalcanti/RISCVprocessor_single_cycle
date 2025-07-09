342829b17c452cff5d6e88fffca8251516660209
Matheus Cavalcanti de Santana <132709977+Matheus-Cavalcanti@users.noreply.github.com>
2025-07-09 16:11:23 -0300

    Update dmem.vhd

--- src/dmem.vhd
+++ src/dmem.vhd
@@ -1,8 +1,8 @@
 library IEEE;
 use IEEE.NUMERIC_BIT.all;		-- Used for rising_edge function
 
-LIBRARY bibli_1;
-USE bibli_1.riscv_pkg.ALL;
+library bibli_1;
+use bibli_1.RISCV_PKG.all;
 
 entity dmem is
 	port(clk, we: in BIT;
@@ -11,22 +11,20 @@ entity dmem is
 end;
 
 architecture behave of dmem is
-  -- Tipo de memória com 64 palavras de 32 bits
-  type ramtype is array (63 downto 0) of BIT_VECTOR(31 downto 0);
-  signal mem : ramtype := (others => (others => '0'));
 begin
-
-  -- Processo síncrono: escrita e leitura na borda de subida
-  process(clk)
-    variable idx : integer;
-  begin
-    if rising_edge(clk) then
-      idx := BTOI(a(7 downto 2));  -- converte endereço para 0..63
-      if we = '1' then
-        mem(idx) <= wd;            -- escreve
-      end if;
-      rd <= mem(idx);              -- lê (read-after-write se we='1')
-    end if;
-  end process;
-
-end architecture;
+	process is
+		type ramtype is array (63 downto 0) of
+		BIT_VECTOR(31 downto 0);
+		variable mem: ramtype;
+	begin
+		-- read or write memory
+		loop
+		if rising_edge(clk) then
+			if (we = '1') then mem(to_integer(unsigned(a(7 downto 2)))) := wd;
+			end if;
+		end if;
+		rd <= mem(to_integer(unsigned(a(7 downto 2))));
+		wait on clk, a;
+		end loop;
+	end process;
+end;
