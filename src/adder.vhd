051b538a7ad10c19c567fbe5dfcd7922ff0c43d7
Matheus Cavalcanti de Santana <132709977+Matheus-Cavalcanti@users.noreply.github.com>
2025-07-09 16:04:27 -0300

    Update adder.vhd

--- src/adder.vhd
+++ src/adder.vhd
@@ -1,8 +1,8 @@
 ENTITY adder IS
-	GENERIC (n		: INTEGER := 32);						-- n of bits
-	PORT 	  (a, b  : IN BIT_VECTOR (n-1 DOWNTO 0);	-- inputs (n bits)
+	GENERIC (width	: INTEGER := 32);						-- n of bits
+	PORT 	  (a, b  : IN BIT_VECTOR (width-1 DOWNTO 0);	-- inputs (n bits)
 				cin	: IN BIT;								-- carry-in
-				y		: OUT BIT_VECTOR (n-1 DOWNTO 0); -- output (n bits)
+				y		: OUT BIT_VECTOR (width-1 DOWNTO 0); -- output (n bits)
 				cout  : OUT BIT );							-- carry-out
 END adder;
 
@@ -13,14 +13,14 @@ COMPONENT som_1a					  -- 1 bit adder component
 			y, cout	 : OUT BIT ); -- output and carry-out
 END COMPONENT;
 
-SIGNAL v : BIT_VECTOR (n DOWNTO 0); -- internal signal to internal carries
+SIGNAL v : BIT_VECTOR (width DOWNTO 0); -- internal signal to internal carries
 
 BEGIN
 
 	v(0) <= cin;  -- in the 1st iteration v(i=0) is the carry-in 
-	cout <= v(n); -- the carry-out receives the last internal carry-out
+	cout <= v(width); -- the carry-out receives the last internal carry-out
 	
-	sum: FOR i IN 0 TO n-1 GENERATE
+	sum: FOR i IN 0 TO width-1 GENERATE
 		element: som_1a PORT MAP (a => a(i),b => b(i),cin => v(i),y => y(i), cout => v(i+1));
 	END GENERATE sum;
 
